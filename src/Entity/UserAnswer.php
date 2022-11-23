7d91afbba1a80f4455597005b5f997f3d51e4791
Hugo-Latreille <hugo.latreille@gmail.com>
2022-11-23 19:44:27 +0100

    :rocket:feat: add all fixtures

--- src/Entity/UserAnswer.php
+++ src/Entity/UserAnswer.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserAnswerRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class UserAnswer
 {
     use TimestampableEntity;
