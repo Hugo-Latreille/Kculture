91204b53b1cffce1979750179090167bf9a9e690
Hugo-Latreille <hugo.latreille@gmail.com>
2022-11-25 15:19:11 +0100

    :rocket:feat: init LexikJWT + config

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -46,7 +46,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $email = null;
 
     #[ORM\Column]
-    #[Groups('get:Users')]
+    #[Groups(['get:Users', 'post:User'])]
     private array $roles = [];
 
     /**
